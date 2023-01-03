cc1b82a26ba7628b9473be2da3c7066200a0bc59
Sajid Ansari <112892616+saajidansarii@users.noreply.github.com>
2023-01-04 01:23:58 +0530

    VS code
    Callback hell countdown and display some msg

---
+++ script.js
@@ -0,0 +1,59 @@
+let n = 10;
+
+const guvi=()=>{
+    setTimeout(()=>{
+        document.getElementById("num").innerHTML = n--;
+        setTimeout(()=>{
+            document.getElementById("num").innerHTML = n--;
+            setTimeout(()=>{
+                document.getElementById("num").innerHTML = n--;
+                setTimeout(()=>{
+                    document.getElementById("num").innerHTML = n--;
+                    setTimeout(()=>{
+                        document.getElementById("num").innerHTML = n--;
+                        setTimeout(()=>{
+                            document.getElementById("num").innerHTML = n--;
+                            setTimeout(()=>{
+                                document.getElementById("num").innerHTML = n--;
+                                setTimeout(()=>{
+                                    document.getElementById("num").innerHTML = n--;
+                                    setTimeout(()=>{
+                                        document.getElementById("num").innerHTML = n--;
+                                        setTimeout(()=>{
+                                            document.getElementById("num").innerHTML = n--;
+                                            setTimeout(()=>{
+                                                document.getElementById("num").innerHTML = n--;
+                                                setTimeout(()=>{
+                                                    document.getElementById("num").innerHTML=("Happy Independence Day");
+                                                },1000);
+                                            },1000);
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+        
+    },2000); 
+}
+guvi(n);
+
+
+// setTimeout(()=>{
+//     document.getElementById("num").innerHTML = n--;
+// },2000);
+// setTimeout(()=>{
+//     document.getElementById("num").innerHTML = n--;
+// },2000);
+// setTimeout(()=>{
+//     document.getElementById("num").innerHTML = n--;
+// },2000);
+// setTimeout(()=>{
+//     document.getElementById("num").innerHTML = n--;
+// },2000);
+// setTimeout(()=>{
+//     document.getElementById("num").innerHTML = n--;
+// },2000);
